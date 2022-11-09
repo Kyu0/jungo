a3899b3af1c258ea36706ebef9ddafa743b7cf4a
Kyu0 <efftar@naver.com>
2022-11-09 23:12:25 +0900

    Initial Commit

---
+++ src/main/java/com/kyu0/jungo/JungoApplication.java
@@ -0,0 +1,13 @@
+package com.kyu0.jungo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyu0/jungo/JungoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyu0.jungo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
