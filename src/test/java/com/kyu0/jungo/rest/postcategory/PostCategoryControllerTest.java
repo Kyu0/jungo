35ea6fc903619651f0458a08254cbfb4ae6d4e93
Kyu0 <efftar@naver.com>
2022-12-14 14:46:52 +0900

    [test] JPA Auditing 기능 추가로 인해 테스트 안되던 문제 해결 @MockBean(JpaMetamodelMappingContext.class) 추가

--- src/test/java/com/kyu0/jungo/rest/postcategory/PostCategoryControllerTest.java
+++ src/test/java/com/kyu0/jungo/rest/postcategory/PostCategoryControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +28,7 @@ import com.kyu0.jungo.rest.postcategory.PostCategory.ModifyRequest;
 import com.kyu0.jungo.rest.postcategory.PostCategory.SaveRequest;
 
 @WebMvcTest(PostCategoryApiController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 public class PostCategoryControllerTest {
     
     private final static String URL = "/api/post-category";
