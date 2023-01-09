a2d1df4a78790a4ee87bd4eca12b4498f2dd3c42
Kyu0 <efftar@naver.com>
2023-01-09 16:57:31 +0900

    [fix] 불필요한 import 제거

--- src/main/java/com/kyu0/jungo/rest/comment/CommentApiController.java
+++ src/main/java/com/kyu0/jungo/rest/comment/CommentApiController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
